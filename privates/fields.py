7035264f76069ac05c7c272161b6e81bcc4c28af
Sergei Maertens <sergei@maykinmedia.nl>
2023-01-03 12:02:00 +0100

    :art: Refactor file fields to use a mixin

--- privates/fields.py
+++ privates/fields.py
@@ -3,13 +3,15 @@ from django.db.models import FileField, ImageField
 from .storages import private_media_storage
 
 
-class PrivateMediaFileField(FileField):
+class PrivateMediaFieldMixin:
     def __init__(self, *args, **kwargs):
         kwargs.setdefault("storage", private_media_storage)
         super().__init__(*args, **kwargs)
 
 
-class PrivateMediaImageField(ImageField):
-    def __init__(self, *args, **kwargs):
-        kwargs.setdefault("storage", private_media_storage)
-        super().__init__(*args, **kwargs)
+class PrivateMediaFileField(PrivateMediaFieldMixin, FileField):
+    pass
+
+
+class PrivateMediaImageField(PrivateMediaFieldMixin, ImageField):
+    pass
