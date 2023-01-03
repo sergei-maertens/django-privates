1e9512fa2bba9698a7b96a6fb668f08d10d941a0
Sergei Maertens <sergei@maykinmedia.nl>
2023-01-03 12:15:30 +0100

    :bookmark: Bump version to 1.5.0

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ copyright = "2018, Sergei Maertens"
 author = "Sergei Maertens"
 
 # The full version, including alpha/beta/rc tags
-release = "1.4.0"
+release = "1.5.0"
 
 
 # -- General configuration ---------------------------------------------------
